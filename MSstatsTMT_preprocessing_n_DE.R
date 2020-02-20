1c83e1914746292a75a712ebec6cc10ad133a41c
MiguelCos <migueljcc5@gmail.com>
2020-02-20 15:10:56 +0100

    first commit

---
+++ MSstatsTMT_preprocessing_n_DE.R
@@ -0,0 +1,10 @@
+##########################################################################
+### Pre-processing and differential analysis of TMT-labelled data ########
+### Based on MSstatsTMT for MaxQuant outputs                      ########
+### Miguel Cosenza v 0.1                                          ########
+##########################################################################
+
+# Requirements: Data to process -----
+
+# Requirements: Your parameters -----
+
